ff88809604dced80e530a082a27ea9b7a063ed35
appukappu <aparnareddy312@gmail.com>
2024-01-02 16:27:41 +0530

    Initial commit

---
+++ src/main/java/org/example/Transfer.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Transfer {
+    public static void main(String[]args){
+        Break B = new Break();
+        Glass g = new Glass();
+        Tyers t = new Tyers();
+        B.bb();
+        g.glass();
+        t.tt();
+    }
+}
